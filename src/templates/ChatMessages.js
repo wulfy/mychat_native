5fb9cb1b30b8504eec73a8d83ff4d11ac9bc6560
Ludovic Lasry <llasry@ludo-mac.home>
2017-03-11 15:49:51 +0100

    chatmessage with ListView

--- src/templates/ChatMessages.js
+++ src/templates/ChatMessages.js
@@ -6,34 +6,44 @@ import {
   TextInput,
   Image,
   View,
-  ScrollView
+  ScrollView,
+  ListView
 } from 'react-native';
 
 export default connect(state => ({ message: state.message}))(
  class ChatMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
   render() 
   {
   	let messages = this.props.message ? this.props.message.messages : [];
   	messages = typeof messages != "undefined" ? messages  : [];
-
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var datasource = ds.cloneWithRows(messages);
     console.log("rendering message");
     console.log(messages);
     console.log("render");
     //utiliser un composant pour interprêter le contenu du serveur (et essayer de ne pas avoir à s'en soucier ici)
     return (
-      <ScrollView>
+      <View>
           	<Text> Last messages : </Text>
-          	{messages.map((data,index)=> 
+
+            <ListView
+              dataSource={datasource}
+              renderRow={(data) => 
               <View>
                 <View style={{float:'left',width:'20%',height:50, backgroundColor:'skyblue'}}>
-                  <Text id={index} className="message" style={data.style}>{data.login}</Text> 
+                  <Text className="message" style={data.style}>{data.login}</Text> 
                 </View>
                 <View style={{float:'right',width:'70%',height:50,backgroundColor:'powderblue'}}>
                   {data.message}
                 </View>
               </View>
-            )}
-      </ScrollView>
+            }
+            />
+      </View>
 
     );
   }
